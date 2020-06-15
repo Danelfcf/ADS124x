a37436251b064c834d15cae263cc16633011dbd5
welvins <johnwelvins@gmail.com>
2020-06-15 18:59:56 -0300

    add ad1.Initialize(); and EndCOM();

--- examples/Internal_Temp/Internal_Temp.ino
+++ examples/Internal_Temp/Internal_Temp.ino
@@ -15,11 +15,14 @@ void setup() {
   // ad1.ResetSW(); // software reset
     
   ad1.SetupInternalTemp(); // Set ads124x to get ADS's internal temperature
+  ad1.EndCOM();
 }
 
 void loop() {
+  ad1.Initialize(); // start spi for ADS
   Serial.print("internal Temp. : ");
   // double inTemp(int scale) 0 = F (not implemented); 1 = C; 2 = K(not implemented will return in C)
   Serial.println(ad1.inTemp(1));
+  ad1.EndCOM(); //leave the SPI bus free for others devices
   delay(100);
 }
